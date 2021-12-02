07cd7cae0b2d555b8e56a66520aacb37c7e0716c
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2021-12-02 11:20:45 +0100

    opsætning

---
+++ NSC-TournamentGen.Core/NSC-TournamentGen.Core/Models/TournamentInput.cs
@@ -0,0 +1,9 @@
+﻿namespace NSC_TournamentGen.Core.Models
+{
+    public class TournamentInput
+    {
+        public int AmountOfParticipants { get; set; }
+        public int AmountOfGroups { get; set; }
+        public int AmountOfRoundsInGroupStage { get; set; }
+    }
+}

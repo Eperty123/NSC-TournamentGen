d265e1874087777cb9d737225dfc981a958e6dc2
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2021-12-02 12:19:19 +0100

    ctx to repo tournament

--- NSC-TournamentGen.DataAccess/Repositories/TournamentRepository.cs
+++ NSC-TournamentGen.DataAccess/Repositories/TournamentRepository.cs
@@ -5,6 +5,13 @@ namespace NSC_TournamentGen.DataAccess.Repositories
 {
     public class TournamentRepository : ITournamentRepository
     {
+        private readonly MainDbContext _ctx;
+
+        public TournamentRepository(MainDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        
         public Tournament ReadTournament(int id)
         {
             throw new System.NotImplementedException();

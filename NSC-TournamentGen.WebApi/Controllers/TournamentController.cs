68cd5788a111327a088bfd898e8a6db3b08c3b3a
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2021-12-02 11:56:58 +0100

    dtos - og controller (tournaments)

---
+++ NSC-TournamentGen.WebApi/Controllers/TournamentController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSC_TournamentGen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TournamentController : ControllerBase
+    {
+    }
+}

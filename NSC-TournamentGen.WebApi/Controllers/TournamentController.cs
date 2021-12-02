24534b0e9d31ae24c4525ea696f9159286bdd175
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2021-12-02 12:38:47 +0100

    service injection i controller

--- NSC-TournamentGen.WebApi/Controllers/TournamentController.cs
+++ NSC-TournamentGen.WebApi/Controllers/TournamentController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NSC_TournamentGen.Domain.Services;
+using NSC_TournamentGen.Dtos;
 
 namespace NSC_TournamentGen.Controllers
 {
@@ -11,5 +13,29 @@ namespace NSC_TournamentGen.Controllers
     [ApiController]
     public class TournamentController : ControllerBase
     {
+        private readonly TournamentService _tournamentService;
+
+        public TournamentController(TournamentService tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+        
+        
+        // GET api/Tournament/5 -- READ By Id
+        [HttpGet("{id}")]
+        public ActionResult<TournamentDto> Get(int id)
+        {
+            if (id < 1) return BadRequest("Id must be greater then 0!");
+
+            // Id ok, proceed!
+            var foundTournament = _tournamentService.GetTournament(id);
+            if (foundTournament != null) return Ok(new TournamentDto()
+            {
+                Id = foundTournament.Id,
+                Name = foundTournament.Name,
+                Participants = foundTournament.Participants
+            });
+            else return StatusCode(500, "User not found.");
+        }
     }
 }

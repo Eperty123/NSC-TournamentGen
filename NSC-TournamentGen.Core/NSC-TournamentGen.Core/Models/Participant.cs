2ded8370a9fa961170c34e401509e2d1d93bd9de
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2021-12-14 15:06:27 +0100

    nu mangler tournament manager

--- NSC-TournamentGen.Core/NSC-TournamentGen.Core/Models/Participant.cs
+++ NSC-TournamentGen.Core/NSC-TournamentGen.Core/Models/Participant.cs
@@ -1,8 +1,12 @@
-﻿namespace NSC_TournamentGen.Core.Models
+﻿using System.Collections.Generic;
+
+namespace NSC_TournamentGen.Core.Models
 {
     public class Participant
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Bracket> BracketsParticipants1 { get; set; }
+        public List<Bracket> BracketsParticipants2 { get; set; }
     }
 }
